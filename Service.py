21eaa35c2c6370d8597a369f64b38f18e1a37aff
Shuhas <Shuhas@github.com>
2022-01-04 11:58:34 +0500

    start func tayyor

--- Service.py
+++ Service.py
@@ -12,13 +12,26 @@ def create_table():
     cur.execute("""CREATE TABLE "noutbuk" ("id"	INTEGER, "brand_id"	TEXT NOT NULL, "nomi" TEXT NOT NULL,
     "cpu"	TEXT NOT NULL, "gpu"	TEXT NOT NULL, "ekran"	TEXT NOT NULL, "ram" TEXT NOT NULL,
     "rom"	TEXT NOT NULL, "narxi" INTEGER NOT NULL, "rang"	TEXT NOT NULL, "garant"	INTEGER NOT NULL,
-    PRIMARY KEY("id"), FOREIGN KEY("brand_id") REFERENCES "brand")""")
-    cur.execute("""CREATE TABLE "user" ("id" INTEGER, "username" TEXT NOT NULL, "ism" TEXT NOT NULL,
-    "raqam"	INTEGER NOT NULL, PRIMARY KEY("id"))""")
+    PRIMARY KEY("id" AUTOINCREMENT), FOREIGN KEY("brand_id") REFERENCES "brand");""")
+    cur.execute("""CREATE TABLE "user" ("id" INTEGER, "user_id" INTEGER NOT NULL UNIQUE, "username" TEXT NOT NULL,
+    "ism" TEXT, "raqam" INTEGER, PRIMARY KEY("id" AUTOINCREMENT));""")
     con.commit()
 
 
 def insert_into():
     cur.execute("""INSERT INTO brand (nomi) values ("Acer"), ("Asus"), ("Dell"), ("HP"), ("Lenovo"),
-                ("MSI"), ("Razer"), ("Samsung"), ("LG"), ("Microsoft Surface"), ("Apple")""")
+                ("MSI"), ("Razer"), ("Samsung"), ("LG"), ("Microsoft Surface"), ("Apple");""")
     con.commit()
+
+def get_one(pk):
+    cur.execute(f"select * from user where user_id")
+    root = cur.fetchone()
+    return root
+
+
+def create_user(user_id, username):
+    sql = f"insert into user (user_id, username) values ({user_id}, '{username}')"
+    cur.execute(sql)
+    con.commit()
+    return get_one(user_id)
+
--- main.py
+++ main.py
@@ -8,3 +8,29 @@ try:
     insert_into()
 except Exception as e:
     pass
+
+
+def start(update, context):
+    user = update.message.from_user
+    if get_one(user.id):
+        pass
+    else:
+        create_user(user_id=user.id, username=user.username)
+    update.message.reply_text("Assalomu alaykum ismingizni kiriting")
+
+def recieved_message(update, context):
+    pass
+
+
+def main():
+    TOKEN = "5060155479:AAGjHg8InuzHNDHrJnEeq8rr7x32QSA09DM"
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
